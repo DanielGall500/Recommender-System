c1e9467554b96f8361dc106cc79981c2f7c421b8
DannyGotTea <danielgallagher500@gmail.com>
2016-05-22 08:58:16 -0700

    rating_set definition

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -12,6 +12,28 @@ movies_data = pd.read_csv(movieinfo_file, sep='|')
 rating_samples, rating_features = ratings_data.shape
 movie_samples, movie_features = movies_data.shape
 
+
+def ratings_set(users, movies):
+    user_info = {}
+
+    for idx, user in users.iterrows():
+        user_id = user['user_id']
+        rating = user['rating']
+        movie_id = user['item_id']
+
+        if user_id in user_info:
+            user_info[user_id].append([movie_id, rating])
+        else:
+            user_info[user_id] = [[movie_id, rating]]
+            print("New user added: {}".format(user_id))
+
+    return user_info
+
+user_ratings = ratings_set(ratings_data, movies_data)
+
+
+
+"""
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 
@@ -20,7 +42,7 @@ pca = PCA().fit_transform(ratings_data)
 clstr = KMeans(n_clusters=(12), max_iter=300, random_state=42)
 
 clstr.fit(pca)
-
+"""
 
 
 
